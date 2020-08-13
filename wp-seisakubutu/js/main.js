a9804bed6e0e14fe625fc73dba57d319a196b44f
takushisato <takuchan19820801@yahoo.co.jp>
2020-08-13 14:45:51 +0900

    wp-seisakubutu

---
+++ wp-seisakubutu/js/main.js
@@ -0,0 +1,11 @@
+jQuery(function() {
+	$('a[href^="#"]').click(function() {
+		var speed = 400;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top-60;
+		$('body, html').animate({scrollTop:position}, speed, 'swing');
+		return false;
+	});
+	
+});
